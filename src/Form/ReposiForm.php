8a31e84e20482ea9215de00115394d327007dcb3
jdlara <jdlara@unicauca.edu.co>
2018-03-06 14:57:13 -0500

    first commit jaja

---
+++ src/Form/ReposiForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\reposi\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormState;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements an example form.
+ */
+class TestFormula extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'reposi_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) 
+  {
+  
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+ 
+  }
+ 
+}
+
+
---
+++ src/Form/reposi_chap_book_form.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\reposi\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormState;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\reposi\Controller\Reposi_info_publication;
+
+/**
+ * Implements an example form.
+ */
+class reposi_chap_book_form extends FormBase {
+
+public function getFormId() {
+    return 'chap_book_form';
+  }
+
+
+public function buildForm(array $form, FormStateInterface $form_state) {
+
+}
+public function validateForm(array &$form, FormStateInterface $form_state) {
+
+  }
+public function submitForm(array &$form, FormStateInterface $form_state) {
+
+}
+}
